05b929a3314aab3d7268b4e59165ddb159437e45
Hana El Sherbeny <apple@apples-mbp-2.home>
2022-01-23 15:08:17 +0000

    Jan23

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 
 def index (request):
-    html="Rango says hey there partner! "+"<a href='/rango/'>Index</a>"
+    html="Rango says hey there partner! "+"<a href='/rango/about/'>About</a>"
     return HttpResponse(html)
 
 def about(request):
-    html= 'Rango says here is the about page. '+"<a href='/rango/about/'>About</a>"
+    html= 'Rango says here is the about page. ' +"<a href='/rango/'>Index</a>"
     return HttpResponse(html)
 
