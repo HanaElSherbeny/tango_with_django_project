4f6ba57564f4a43f7b1b27da7a97c8f621631438
Hana El Sherbeny <apple@apples-mbp-2.home>
2022-01-29 15:58:15 +0000

    jan 29

--- populate_rango.py
+++ populate_rango.py
@@ -19,24 +19,27 @@ def populate():
 # Then we will create a dictionary of dictionaries for our categories. # This might seem a little bit confusing, but it allows us to iterate # through each data structure, and add the data to our models.
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         "views":100},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         "views":102},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         "views":150} ]
     django_pages = [
  
  {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',"views":99},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',"views":102},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/',"views":90} ]
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',"views":99},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org',"views":89} ]
     cats = {'Python': {'pages': python_pages,'views':128, 'likes':64},
             'Django': {'pages': django_pages,'views': 64, 'likes': 32 },
             'Other Frameworks': {'pages': other_pages,'views':32 , 'likes': 16} }
@@ -46,7 +49,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])#
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])#
             
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -59,6 +62,7 @@ def add_page(cat,title,url,views=0):
     p.views=views
     p.save()
     return p
+
 def add_cat(name,views=0,likes=0):
     
     c = Category.objects.get_or_create(name=name)[0] 
--- rango/urls.py
+++ rango/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path("index/", views.index, name='index'),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
+
+ 
