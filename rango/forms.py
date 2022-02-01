ea3deb691318cfc3bf7bf675b7746a21f58c3611
Hana El Sherbeny <apple@apples-mbp-2.home>
2022-02-01 23:54:53 +0000

    feb 1 3

--- rango/forms.py
+++ rango/forms.py
@@ -13,6 +13,14 @@ class CategoryForm(forms.ModelForm):
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    def clean(self):
+        cleaned_data = self.cleaned_data 
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://', # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
  
     # An inline class to provide additional information on the form.
     class Meta:
--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ Created on Fri Jan 21 16:41:02 2022
 
 @author: apple
 """
-from django.conf.urls import url
+
 from django.urls import path 
 from rango import views
 app_name = 'rango'
@@ -18,7 +18,7 @@ urlpatterns = [
     path('category/<slug:category_name_slug>/',
          views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+    path('/rango/category/add_page/', views.add_page, name='add_page'),
 
 ]
 
