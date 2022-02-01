8ce3e534f66f50b64f719af5a0ab5d14fd741bd8
Hana El Sherbeny <apple@apples-mbp-2.home>
2022-02-01 21:16:25 +0000

    feb1 2

--- rango/forms.py
+++ rango/forms.py
@@ -9,8 +9,7 @@ Created on Sun Jan 30 17:04:47 2022
 from django import forms 
 from rango.models import Page,Category
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-           help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -23,11 +22,18 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data 
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://', # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
 class Meta:
     # Provide an association between the ModelForm and a model model = Page
     # What fields do we want to include in our form?
